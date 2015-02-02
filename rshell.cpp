f32e0a336728099e74ab8f68eedc382bf3465b8e
Wolfgang Finkbeiner <wfink001@hammer.cs.ucr.edu>
2015-02-02 00:13:12 -0800

    copied example from forking from github to modify for exec calls.

--- rshell.cpp
+++ rshell.cpp
@@ -1,6 +1,26 @@
 #include <iostream>
+#include <stdio.h>, #include <errno.h>
 
 int main(){
 
+  int pid = fork();
+    if(pid == -1)//fork’s return value for an error is -1
+    {
+       //perror("There was an error with fork(). ");
+       perror("fork"); //although you certainly can use the above, it is good
+                       //practice not to write more information than necessary
+       exit(1);//there was an error with fork so exit the program and go back and fix it
+    }
+    else if(pid == 0)//when pid is 0 you are in the child process
+    {
+       cout<<"This is the child process ";
+       exit(1);  //when the child process finishes doing what we want it to, cout,
+                 //we want to kill the child process so it doesn’t go on in the program so we exit
+    }
+    else if(pid > 0){//if pid is not 0 then we’re in the parent
+        if (-1 == wait(0)
+            perror("wait");
+
+    }
     return 0;
 }
