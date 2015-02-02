46b0bf7343953e7813009b8f87769bdfafe90380
Wolfgang Finkbeiner <wfink001@hammer.cs.ucr.edu>
2015-02-02 15:10:02 -0800

    code works for test argv

--- rshell.cpp
+++ rshell.cpp
@@ -1,7 +1,21 @@
 #include <iostream>
-#include <stdio.h>, #include <errno.h>
-
+#include <stdio.h>
+#include <errno.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+#include <cstring>
+#include <cstdlib>
 int main(){
+
+
+
+   char* argv[3];
+   argv[0] = "ls";
+
+   argv[1] =  "-t";
+   argv[2] =  "-l";
+
 
   int pid = fork();
     if(pid == -1)//fork’s return value for an error is -1
@@ -13,12 +27,15 @@ int main(){
     }
     else if(pid == 0)//when pid is 0 you are in the child process
     {
-       cout<<"This is the child process ";
+
+       if (0 < execvp(argv[0], argv))
+           perror("execvp");
+
        exit(1);  //when the child process finishes doing what we want it to, cout,
                  //we want to kill the child process so it doesn’t go on in the program so we exit
     }
     else if(pid > 0){//if pid is not 0 then we’re in the parent
-        if (-1 == wait(0)
+        if (-1 == wait(0))
             perror("wait");
 
     }
