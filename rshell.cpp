454c06149a05ae094399d1bd68d4ac0c7e08f7dc
Wolfgang Finkbeiner <wfink001@well.cs.ucr.edu>
2015-02-02 21:29:19 -0800

    organized for easier syscalling

--- rshell.cpp
+++ rshell.cpp
@@ -7,6 +7,38 @@
 #include <sys/wait.h>
 #include <cstring>
 #include <cstdlib>
+
+
+int fcall(char* argv[]){
+    if (!strcmp(argv[0], "exit")){
+
+        return 1;
+    }
+
+    int pid = fork();
+    if(pid == -1)//fork’s return value for an error is -1
+    {
+       //perror("There was an error with fork(). ");
+       perror("fork"); //although you certainly can use the above, it is good
+                       //practice not to write more information than necessary
+       exit(1);//there was an error with fork so exit the program and go back and fix it
+    }
+    else if(pid == 0)//when pid is 0 you are in the child process
+    {
+
+       if (0 < execvp(argv[0], argv))
+           perror("execvp");
+
+       exit(1);  //when the child process finishes doing what we want it to, cout,
+                 //we want to kill the child process so it doesn’t go on in the program so we exit
+    }
+    else if(pid > 0){//if pid is not 0 then we’re in the parent
+        if (-1 == wait(0))
+            perror("wait");
+    }
+    return 0;
+}
+
 int main(){
 
     while(1){
@@ -25,38 +57,9 @@ int main(){
             pch = strtok(NULL, " ");
         }
 
-
-       /*char* argv[3];
-       argv[0] = "ls";
-
-       argv[1] =  "-t";
-       argv[2] =  "-l";
-        */
-        if (!strcmp(argv[0], "exit")){
-            delete [] cstr;
+        if (fcall(argv)){
+            delete[] cstr;
             goto skippy;
-        }
-
-        int pid = fork();
-        if(pid == -1)//fork’s return value for an error is -1
-        {
-           //perror("There was an error with fork(). ");
-           perror("fork"); //although you certainly can use the above, it is good
-                           //practice not to write more information than necessary
-           exit(1);//there was an error with fork so exit the program and go back and fix it
-        }
-        else if(pid == 0)//when pid is 0 you are in the child process
-        {
-
-           if (0 < execvp(argv[0], argv))
-               perror("execvp");
-
-           exit(1);  //when the child process finishes doing what we want it to, cout,
-                     //we want to kill the child process so it doesn’t go on in the program so we exit
-        }
-        else if(pid > 0){//if pid is not 0 then we’re in the parent
-            if (-1 == wait(0))
-                perror("wait");
         }
     }
     skippy:
