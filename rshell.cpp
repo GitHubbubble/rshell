c4bbf0f7318682a2b127fb52021efe24327756d2
Wolfgang Finkbeiner <wfink001@well.cs.ucr.edu>
2015-02-02 21:13:33 -0800

    super edge case exit works

--- rshell.cpp
+++ rshell.cpp
@@ -9,51 +9,57 @@
 #include <cstdlib>
 int main(){
 
-    std::string chunky;
-    std::getline(std::cin, chunky);
+    while(1){
+        std::string chunky;
+        std::getline(std::cin, chunky);
 
-    char *cstr = new char [chunky.length() + 1];
-    strcpy(cstr, chunky.c_str());
+        char *cstr = new char [chunky.length() + 1];
+        strcpy(cstr, chunky.c_str());
 
-    char*  pch;
-    char* argv[1024];
-    pch = strtok(cstr, " ");
+        char*  pch;
+        char* argv[1024];
+        pch = strtok(cstr, " ");
 
-    for( int p = 0; pch != NULL; p++){
-        argv[p] = pch;
-        pch = strtok(NULL, " ");
+        for( int p = 0; pch != NULL; p++){
+            argv[p] = pch;
+            pch = strtok(NULL, " ");
+        }
+
+
+       /*char* argv[3];
+       argv[0] = "ls";
+
+       argv[1] =  "-t";
+       argv[2] =  "-l";
+        */
+        if (!strcmp(argv[0], "exit")){
+            delete [] cstr;
+            goto skippy;
+        }
+
+        int pid = fork();
+        if(pid == -1)//fork’s return value for an error is -1
+        {
+           //perror("There was an error with fork(). ");
+           perror("fork"); //although you certainly can use the above, it is good
+                           //practice not to write more information than necessary
+           exit(1);//there was an error with fork so exit the program and go back and fix it
+        }
+        else if(pid == 0)//when pid is 0 you are in the child process
+        {
+
+           if (0 < execvp(argv[0], argv))
+               perror("execvp");
+
+           exit(1);  //when the child process finishes doing what we want it to, cout,
+                     //we want to kill the child process so it doesn’t go on in the program so we exit
+        }
+        else if(pid > 0){//if pid is not 0 then we’re in the parent
+            if (-1 == wait(0))
+                perror("wait");
+        }
     }
-
-
-   /*char* argv[3];
-   argv[0] = "ls";
-
-   argv[1] =  "-t";
-   argv[2] =  "-l";
-    */
-
-  int pid = fork();
-    if(pid == -1)//fork’s return value for an error is -1
-    {
-       //perror("There was an error with fork(). ");
-       perror("fork"); //although you certainly can use the above, it is good
-                       //practice not to write more information than necessary
-       exit(1);//there was an error with fork so exit the program and go back and fix it
-    }
-    else if(pid == 0)//when pid is 0 you are in the child process
-    {
-
-       if (0 < execvp(argv[0], argv))
-           perror("execvp");
-
-       exit(1);  //when the child process finishes doing what we want it to, cout,
-                 //we want to kill the child process so it doesn’t go on in the program so we exit
-    }
-    else if(pid > 0){//if pid is not 0 then we’re in the parent
-        if (-1 == wait(0))
-            perror("wait");
-
-    }
-    delete [] cstr;
+    skippy:
+    std::cout << "Ar revoir!\n";
     return 0;
 }
