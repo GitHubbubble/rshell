48e292318d6797b639ef81000ba9f23022b98526
Hubbubble <wfink001@ucr.edu>
2015-02-08 14:36:13 -0800

    Perror for opendir, and begining on parser

--- ls.cpp
+++ ls.cpp
@@ -1,4 +1,4 @@
-nclude <sys/types.h>
+#include <sys/types.h>
 #include <dirent.h>
 #include <errno.h>
 
@@ -11,13 +11,47 @@ using namespace std;
    * that there is no error checking on these functions.  You MUST add error
    * checking yourself.
  */
+void lstester(char a, char l, char R){
+    cout << (int)a << endl;
+    cout << (int)l << endl;
+    cout << (int)R << endl
+}
 
-int main()
+void lsrunner(char a, char l, char R, char* nam){
+    char *dirName = nam ;
+    DIR *dirp
+    if ((dirp = opendir(dirNamei))== NULL ){
+        perror("Open dir");
+        exit(1);
+
+    }
+
+    dirent *direntp;
+    while ((direntp = readdir(dirp)))
+
+        cout << direntp->d_name << "  " ;  // use stat here to find attributes of file
+    closedir(dirp);
+
+    cout << endl;
+}
+
+int main(int argc, char *argv[])
 {
-        char *dirName = ".";
-        DIR *dirp = opendir(dirName);
-        dirent *direntp;
-        while ((direntp = readdir(dirp)))
-            cout << direntp->d_name << endl;  // use stat here to find attributes of file
-        closedir(dirp);
+    char fa;
+    char fl;
+    char fR;
+
+
+    for(int i = 0; int i < (int)argc; i++){
+
+        char str[] =argv[i] ;
+        char * pch;
+        printf ("Splitting string \"%s\" into tokens:\n",str);
+        pch = strtok (str," ,.-");
+        while (pch != NULL){
+         printf ("%s\n",pch);
+        pch = strtok (NULL, " ,.-");
+          }
+    }
+
 }
